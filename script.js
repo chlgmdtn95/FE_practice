2c60057afd72f6007d24b67db524f9ab0af00bd4
heuchoi <heuchoi@student.42seoul.kr>
2025-03-15 22:58:02 +0900

    ?

--- script.js
+++ script.js
@@ -5,10 +5,28 @@ function createBoxes(count) {
         return;
     for (let i = 0; i < count; i++) {
         const win_lose_box = document.createElement('div');
-        win_lose_box.className = 'col-span-12 pl-2 h-32 bg-red-800 m-2 rounded-xl';
-        const box = document.createElement('div');
-        box.className = 'w-full h-32 bg-white rounded-xl';
-		win_lose_box.append(box)
+		if (i % 2 == 0)
+        	win_lose_box.className = 'col-span-12 pl-2 h-32 bg-red-800 m-2 rounded-xl';
+		else
+			win_lose_box.className = 'col-span-12 pl-2 h-32 bg-blue-800 m-2 rounded-xl';
+		const box = document.createElement('div');
+        box.className = 'w-full h-32 bg-white rounded-xl flex items-center pl-16 gap-16';
+		
+		const oppo_img = document.createElement('img');
+		oppo_img.className = 'bg-red-100 p-1 rounded-full w-24 h-24 object-cover object-center';
+		oppo_img.src = './ai_icon.png';
+
+		const win_lose_txt = document.createElement('p');
+		if (i % 2 == 0)
+			win_lose_txt.innerText = 'WIN';
+		else
+			win_lose_txt.innerText = 'LOSE';
+
+		win_lose_txt.className = 'text-2xl font-bold text-black';
+
+		win_lose_box.append(box);
+		box.append(oppo_img);
+		box.append(win_lose_txt);
         container.appendChild(win_lose_box);
     }
 }
